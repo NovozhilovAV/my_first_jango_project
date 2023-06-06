545516e24d3ce3169935003d30e9a8af2c87de13
NovozhilovAV <tema.reida87@gmail.com>
2023-06-06 20:30:48 +0300

    refresh commit django

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+def index(requesrs):
+    return HttpResponse('<h1>Main page</h1>')
+
+def index_myapp(requests):
+    return HttpResponse('<h1>Myapp Page</h1>')
 def about(requests):     # функциональный метод
     return HttpResponse('about site, plies')
 # request потому как мы отправляем запрос на сайт
@@ -8,5 +14,13 @@ def about(requests):     # функциональный метод
 def login(requests):
     return HttpResponse('<h2>Page login !!!</h2>')
 
-def contacts(requests):
-    return HttpResponse('<h1>Page Contacts</h1>')
+def contacts(requests, id):
+    url_id = id
+    name = requests.GET.get('name')
+    age = requests.GET.get('age')
+            # requests.POST.get('password')    # для примера запроса
+    get_params = {'name': name, 'age': age}
+    # return HttpResponse(f'<h1>Page Contacts</h1>, id = {id}')
+    # return HttpResponse(f'<h1>Page Contacts</h1>, id = {id}, name = {name}, age = {age}')
+    return HttpResponse(f'<h1>Page Contacts</h1>, url_params_id = {url_id}, get_params - {get_params}')
+
